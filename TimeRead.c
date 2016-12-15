134f8a04fcb62c30ab9ce8f5c2ace4fee586af6e
Vincent-SP <vincent.serepeyrigain@outlook.fr>
2016-12-15 00:26:22 -0800

    dynamique .c et .h + main.c

--- TimeRead.c
+++ TimeRead.c
@@ -1,56 +1,64 @@
 #include "TimeRead.h"
 
 
-void printf_center (char sentence[])
+void printf_center (char sentence[]) // fonction de centrage
 {
         unsigned int n;
         unsigned int i;
-        for (i = 0; i < (HEIGHT_TERMINAL-1)/2 ; i++)
+        for (i = 0; i < (HEIGHT_TERMINAL-1)/2 ; i++) // i qui parcour la moitié de la hauteur du terminal
         {
-                putchar('\n');
+                putchar('\n'); // fait les saut de ligne
         }
 
-        for (n = 0 ; n  < (LENGHT_TERMINAL-8)/2 ; n++)
+        for (n = 0 ; n  < (LENGHT_TERMINAL-8)/2 ; n++) // n qui parcour la moitié de la longueur du terminal
         {
-                putchar(' ');
+                putchar(' '); // fait les espaces
         }
-	timeRead();
-	for (n= 0; n< (LENGHT_TERMINAL-(strlen(sentence)))/2 ; n++)
+	timeRead(); //Affiche l'heur
+	for (n= 0; n< (LENGHT_TERMINAL-(strlen(sentence)))/2 ; n++) //n qui parcour la moitié de la longureur du terminal - la taille de la chaine de caractères 
 	{
-		putchar(' ');
+		putchar(' '); //ajoute les espaces
 	}
-	printSentenceRefresh(sentence);
-        for (i = 0; i < (HEIGHT_TERMINAL)/2 ; i++)
+	printSentenceRefresh(sentence); // fonction contenant la phrase
+        for (i = 0; i < (HEIGHT_TERMINAL)/2 ; i++) // i qui parcours l'autre moitié du terminal
         {
-                putchar('\n');
+                putchar('\n'); // ajout des retours à la ligne
         }
 }
 
-void timeRead()
+void timeRead() // fonction qui lit l'heur
 {
-	time_t hour;
+	time_t hour; 
 	struct tm date;
 
 	time(&hour);
 	date = *gmtime(&hour);
 	printf(" %d:%d:%d\n ",(date.tm_hour+FRANCE+24)%24, date.tm_min, date.tm_sec);
+	/*tm_wday, le jour de la semaine est exprimé en 3 lettres dans son nom anglais (ex : Lundi -> Mon);
+	tm_mon, le nom du mois exprimé en 3 lettres dans son anglais (ex : Juin -> Jun) ;
+	tm_mday, le jour du mois;
+	tm_hour, l'heure;
+	tm_min, les minutes;
+	tm_sec, les secondes;
+	tm_year, l'année.
+	*/
 }
 
-void printSentenceRefresh(char sentence[])
+void printSentenceRefresh(char sentence[]) //fonction qui affiche la phrase et les points
 {
-	int j = 0;
+	int j = 0; 
 	printf("%s", sentence);
-	fflush(stdout);
-	while (j<5)
+	fflush(stdout);// Avant d'être afficher, les caractères sont mis en mémoire (tampon) vide cette mémoire et force l'affichage des caractères qui y étaient.
+	while (j<5) //Boucle pour afficher 5 points
 	{
 		printf(".");
-		fflush(stdout);
-		sleep(1);
-		j++;
+		fflush(stdout);//conserve en mémoire tampon puis affiche
+		sleep(1);// attend 1 sec
+		j++; // incrémente j de 1
 	}
 
-system("clear");
-printf_center(sentence);
+system("clear"); // vide la console 
+printf_center(sentence); // affiche la phrase qui est récupéré avec le printf qui est envyé dans la fonction de centrage.
 
 }
 
---
+++ mainTimeRead.c
@@ -0,0 +1,11 @@
+#include "TimeRead.c"//récupère le .c
+#include "TimeRead.h"//récupère le .h
+
+int main()
+{
+        char sentence[] = "Time will be refresh in few seconds"; // permet de récupérer la chaine de caractère dans un tableau
+                system("clear"); //vide la console 
+                printf_center(sentence);// fonction qui affiche le programme, centrage, heur, et phrase
+        return 0;
+}
+
